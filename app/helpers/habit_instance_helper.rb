c49e086567a3ea437267d8d4a35074191f37bb98
Peter <monroepe@gmail.com>
2015-08-02 15:40:38 -0400

    Fixed habit instance helper

--- app/helpers/habit_instance_helper.rb
+++ app/helpers/habit_instance_helper.rb
@@ -3,11 +3,12 @@ module HabitInstanceHelper
 
   def create_habit_instances
     habits = current_user.habits
-    today = Time.new.strftime("%m/%d/%Y")
+    today = Time.new.strftime("%Y-%d-%m")
 
     habits.each do |habit|
-      if !current_user.habit_instances.where(habit_id: habit.id).where(date: today)
+      if current_user.habit_instances.where(habit_id: habit.id).where(date: today) == []
         habit_instance = current_user.habit_instances.build({date: today, habit_id: habit.id, user_id: current_user.id, complete: false})
+        habit_instance.save
       end
     end
   end
